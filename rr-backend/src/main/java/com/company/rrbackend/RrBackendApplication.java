4e1afa87c775c79548d1ca34a27b02a2b4fc9671
Brian Ruiz <98550010+bruizbruiz@users.noreply.github.com>
2023-08-16 21:04:18 -0400

    Project structure

---
+++ rr-backend/src/main/java/com/company/rrbackend/RrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.rrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrBackendApplication.class, args);
+	}
+
+}
---
+++ rr-backend/src/test/java/com/company/rrbackend/RrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.rrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
